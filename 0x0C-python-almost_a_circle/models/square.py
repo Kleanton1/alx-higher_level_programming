e104f0f37c204502669a7138888ab3dd4fe5f4d3
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-08-13 18:25:21 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,61 +1,80 @@
 #!/usr/bin/python3
-"""
-This module contains the "Square" class
-"""
-
+'''Defines a class square that inherits from the Rectangle class
+'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """A representation of a square"""
+    '''Defines a class Square that inherits from Rectangle
+    '''
+
     def __init__(self, size, x=0, y=0, id=None):
-        """initializes the square"""
+        '''Initializes new square object
+
+        Args:
+           size (int): The size of the square to be initialized
+           x (int): The horizontal position of the square
+           y (int): The vaertical position of the square
+           id (int): Instance id
+        '''
         super().__init__(size, size, x, y, id)
-        self.size = size
 
-    @property
-    def size(self):
-        """getter for size"""
-        return self.width
+     def __str__(self):
+         '''Return string representation of a square object
+         '''
+         return(f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
 
-    @size.setter
-    def size(self, value):
-        """setter for size"""
-        self.width = value
-        self.height = value
+      @property
+      def size(self):
+          '''Getter for the size of square instance
+          '''
+          return self.width
 
-    def __str__(self):
-        """informal string representation of the square"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
-                                                         self.y, self.width)
+      @size.setter
+      def size(self, size):
+          '''Setter for size of square object
+
+           Args:
+              size (int): The size to be set for square object
+           '''
+          self.width = size
+          self.height = size
 
     def update(self, *args, **kwargs):
-        """update attributes"""
-        if len(args):
-            for i, a in enumerate(args):
-                if i == 0:
-                    self.id = a
-                elif i == 1:
-                    self.size = a
-                elif i == 2:
-                    self.x = a
-                elif i == 3:
-                    self.y = a
-        else:
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "size" in kwargs:
-                self.size = kwargs["size"]
-            if "x" in kwargs:
-                self.x = kwargs["x"]
-            if "y" in kwargs:
-                self.y = kwargs["y"]
+        '''Updates the instance attribute values with the values specified
+
+         Args:
+            args (list):
+               1st argument should be the id attribute
+               2nd argument should be the size attribute
+               3rd argument should be the x attribute
+               4th argument should be the y attribute
+            kwargs (dict):
+               Contains keyword argument to be updated
+         '''
+        if len(args) > 0:
+            attributes = ["id", "size", "x", "y"]
+            for i, attr in enumerate(args):
+                setattr(self, attributes[i], attr)
+            else:
+                for key in kwargs.keys():
+                    setattr(self, key, kwargs[key])
 
     def to_dictionary(self):
-        """dictionary representation of a Square"""
-        d = {}
-        d["id"] = self.id
-        d["size"] = self.size
-        d["x"] = self.x
-        d["y"] = self.y
-        return d
+        '''returns the dictionary representation of a Rectangle object
+        '''
+        def remove_prefix(value):
+            '''Converts private attribute format to normal attribute name
+
+            Args:
+            value (str): The value to be converted
+            '''
+            if value[:12] == "_Rectangle__":
+                if value[12:] in ["width", "height"]:
+                    return "size"
+                else:
+                    return value[12:]
+                return value
+
+        return {remove_prefix(key): self.__dict__[key] for key in
+                                self.__dict__.keys()}
