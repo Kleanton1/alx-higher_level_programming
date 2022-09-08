4d4fc7eea9a149061285b8b2b2322700b19e98c9
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-09-08 20:48:14 +0000

    No cheating

--- 0x0D-SQL_introduction/12-no-cheating.sql
+++
@@ -1,2 +0,0 @@
--- Updates the table with new values
-UPDATE second_table SET score =10 WHERE name = Bob;
--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- Updates the table with new values
-UPDATE 'second_table' SET 'score'=10 WHERE 'name'="Bob";
+UPDATE second_table SET score =10 WHERE name = Bob;
