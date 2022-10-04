093fb0d7e3e80c0ffbba1c0ea2656fbdeea6eac2
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-10-04 22:30:28 +0000

    Multi-languages_loops

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < lines.lenght; i++) {
-	console.log(lines[i]);
+for (const str of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+	  console.log(str);
 }
