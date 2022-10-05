58891852bacf6f58bb2a57d53d1b609a3386eb77
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-10-05 07:50:57 +0000

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,10 +3,10 @@
 const args = process.argv;
 let num = args[2];
 if (!isNaN(num)) {
-	num = parseInt(num);
-	for (let i = 0; i < num; i++) {
-	console.log('C is fun');
-	}
+  num = parseInt(num);
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 } else {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
