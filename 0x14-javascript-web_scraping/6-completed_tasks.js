afd18e94eb2229281edcd5e33da53c60b5f8c5c1
Kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-11-16 12:06:01 +0000

    computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    const long = data.length;
+
+    const countDict = {};
+
+    for (let i = 0; i < long; i++) {
+      if (data[i].completed) {
+        if (!(data[i].userId in countDict)) {
+          countDict[data[i].userId] = 0;
+        }
+        countDict[data[i].userId] += 1;
+      }
+    }
+    console.log(countDict);
+  }
+});
