92863ee4003a3a68c6423da985bfe5a352a0627c
Kleanton1 <ifeanyiemmanuel271@gamil.com>
2022-11-15 18:34:24 -0800

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const file = process.argv[2];
+
 const fs = require('fs');
 
-fs.readfile(file, 'utf-8', (err, data) => {
+fs.readfile(process.argv[2], 'utf-8',function (err, data) {
   if (err) {
     console.log(err);
-    return;
-  }
+  } else {
   console.log(data);
+  }
 });
